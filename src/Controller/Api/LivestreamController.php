99b60dda9bcc36a9dd7b1609a2b778eb82deac10
Peter Vuyk <p.vuyk@drukwerkdeal.nl>
2018-09-16 13:34:05 +0200

    Make security and swagger documentation work

--- src/Controller/Api/LivestreamController.php
+++ src/Controller/Api/LivestreamController.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 namespace App\Controller\Api;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Swagger\Annotations as SWG;
 
 class LivestreamController
 {
     /**
+     * @SWG\Response(
+     *     response=200,
+     *     description="Returns response if livestream could be started"
+     * )
      * @return JsonResponse
      */
     public function startLivestream()
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -5,11 +5,29 @@ namespace App\Repository;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\NonUniqueResultException;
 use Doctrine\ORM\OptimisticLockException;
 use Doctrine\ORM\ORMException;
+use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
-class UserRepository extends EntityRepository
+class UserRepository extends EntityRepository implements UserLoaderInterface
 {
+    /**
+     * @param string $username
+     * @return mixed|null|UserInterface
+     * @throws NonUniqueResultException
+     */
+    public function loadUserByUsername($username)
+    {
+        return $this->createQueryBuilder('u')
+            ->where('u.username = :username OR u.email = :email')
+            ->setParameter('username', $username)
+            ->setParameter('email', $username)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
+
     /**
      * @param User $user
      * @throws ORMException
