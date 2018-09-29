0f283d55a96bc35a94fd9e5b714d72121de87099
Peter Vuyk <p.vuyk@drukwerkdeal.nl>
2018-09-29 13:45:27 +0200

    Added webpack encore to the project

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const $ = require('jquery');
+require('bootstrap');
+require('bootstrap/dist/css/bootstrap.css');
+require('font-awesome/css/font-awesome.css');
+require('../css/app.css');
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+    .addEntry('app', './assets/js/app.js')
+
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+
+    .autoProvidejQuery()
+;
+
+module.exports = Encore.getWebpackConfig();
